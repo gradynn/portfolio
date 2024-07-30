7af7588c5e047918cf482c40d4c7c7e9da63213d
Gradyn Nagle <gradyndnagle@outlook.com>
2024-07-29 22:04:43 -0700

    added some basic nextjs layout and some empty pages

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+    return (
+        <div className="flex justify-between items-center px-10 py-2 w-full bg-white">
+            <div className="flex items-center">
+            <h1 className="text-4xl font-light">
+                <Link href="/">Gradyn Nagle</Link>
+            </h1>
+            </div>
+            <div className="flex items-center">
+            <Link href="/about/" className="text-base px-5">About</Link>
+            <Link href="/blog/" className="text-base px-5">Blog</Link>
+            <Link href="/contact/" className="text-base px-5">Contact</Link>
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center bg-white text-black">
-      <div className="flex justify-between items-center px-10 py-5 w-full">
-        <div className="flex items-center">
-          <h1 className="text-4xl font-light">
-            Gradyn Nagle
-          </h1>
-        </div>
-        <div className="flex items-center">
-          <p className="text-base px-5">About</p>
-          <p className="text-base px-5">Blog</p>
-          <p className="text-base px-5">Contact</p>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex">
+
+		</main>
+	);
 }
