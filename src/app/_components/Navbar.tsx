936990870ae65422b51d73156e99374182eda512
Gradyn Nagle <gradyndnagle@outlook.com>
2024-08-11 20:56:35 -0700

    navbar indicates page selection

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -4,52 +4,104 @@ import Link from 'next/link';
 import { FiMenu, FiX } from "react-icons/fi";
 import React, { useState } from 'react';
 
-function MobileMenu({ onClose }: { onClose: () => void }) {
+function MobileMenu({ onClose, activePage, setActivePage }: { onClose: () => void, activePage: string,  setActivePage: () => void  }) {
+    const handleClick = (p: string) => {
+        setActivePage(p);
+        onClose();
+    }
+    
     return (
         <div className="absolute inset-0 w-screen h-screen bg-white z-50">
             <div className="flex justify-end px-10 py-5">
                 <FiX onClick={onClose} className="text-4xl cursor-pointer" />
             </div>
             <div className="flex flex-col items-start justify-center mt-10 px-10">
-                <Link onClick={onClose} href="/" className="text-4xl py-5">Home</Link> 
-                <Link onClick={onClose} href="/about/" className="text-4xl py-5">About</Link>
-                <Link onClick={onClose} href="/blog/" className="text-4xl py-5">Blog</Link>
-                <Link onClick={onClose} href="/contact/" className="text-4xl py-5">Contact</Link>
+                <Link 
+                    onClick={() => handleClick("")} 
+                    href="/" 
+                    className={`text-4xl py-5 ${activePage == "" ? "text-blue-500" : ""}`}
+                >
+                    Home
+                </Link> 
+                <Link 
+                    onClick={() => handleClick("About")}
+                    href="/about/"
+                    className={`text-4xl py-5 ${activePage == "About" ? "text-blue-500" : ""}`}
+                >
+                    About
+                </Link>
+                <Link 
+                    onClick={() => handleClick("Blog")}
+                    href="/blog/" 
+                    className={`text-4xl py-5 ${activePage == "Blog" ? "text-blue-500" : ""}`}
+                >
+                    Blog
+                </Link>
+                <Link 
+                    onClick={() => handleClick("Contact")}
+                    href="/contact/"
+                    className={`text-4xl py-5 ${activePage == "Contact" ? "text-blue-500" : ""}`}
+                >
+                    Contact
+                </Link>
             </div>
         </div> 
     )
 }
 
-function FullMenu() {
+function FullMenu({ activePage, setActivePage }: { activePage: string, setActivePage: () => void }) {
+    const handleClick = (p: string) => {
+        setActivePage(p);
+    }
+    
     return (
         <div className="flex items-center">
-            <Link href="/about/" className="text-base pr-5">About</Link>
-            <Link href="/blog/" className="text-base px-5">Blog</Link>
-            <Link href="/contact/" className="text-base pl-5">Contact</Link>
+            <Link 
+                onClick={() => handleClick("About")}
+                href="/about/"
+                className={`text-base pr-5 ${activePage == "About" ? "text-blue-500" : ""}`}
+            >
+                About
+            </Link>
+            <Link 
+                onClick={() => handleClick("Blog")}
+                href="/blog/" 
+                className={`text-base px-5 ${activePage == "Blog" ? "text-blue-500" : ""}`}
+            >
+                Blog
+            </Link>
+            <Link 
+                onClick={() => handleClick("Contact")}
+                href="/contact/"
+                className={`text-base pl-5 ${activePage == "Contact" ? "text-blue-500" : ""}`}
+            >
+                Contact
+            </Link>
         </div>
     )
 }
 
 export default function Navbar() {
     const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
+    const [activePage, setActivePage] = useState<string>("");
 
     return (
         <div className="flex justify-between items-center px-10 py-5 w-full bg-white">
             <div className="flex items-center">
                 <h1 className="text-4xl font-light">
-                    <Link href="/">
+                    <Link onClick={() => setActivePage("")} href="/">
                         Gradyn <span className="md:inline hidden">Nagle</span>
                     </Link>
                 </h1>
             </div>
             <div className="md:block hidden">
-                <FullMenu />
+                <FullMenu activePage={activePage} setActivePage={setActivePage}/>
             </div>
             <div className="md:hidden block">
                 <FiMenu onClick={() => setShowMobileMenu(!showMobileMenu)} className="text-4xl align-baseline" /> 
             </div>
             {showMobileMenu && (
-                <MobileMenu onClose={() => setShowMobileMenu(!showMobileMenu)}/>
+                <MobileMenu onClose={() => setShowMobileMenu(!showMobileMenu)} activePage={activePage} setActivePage={setActivePage}/>
             )}
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Typewriter } from 'react-simple-typewriter';
+import Link from 'next/link';
 
 import VideoBackground from "./_components/VideoBackground/VideoBackground";
 
@@ -12,20 +13,17 @@ const headlines = [
 ];
 
 export default function Home() {
-	const routeToAbout = () => {
-		const url = window.location.href;
-		window.location.href = url + "/about";
-	};
-
 	return (
 		<main className="flex justify-start items-center min-h-screen">
 			<VideoBackground />
 			<div className="flex flex-col justify-center p-10 text-4xl h-screen">
-				<span className="text-red-500"><Typewriter words={headlines} cursor={true} cursorBlinking={true} loop={false} /></span>
+				<span className="text-blue-500"><Typewriter words={headlines} cursor={true} cursorBlinking={true} loop={false} /></span>
 				<div className="w-[100px] border-b border-2 border-black mt-10"></div>
-				<button className="w-[175px] text-2xl bg-red-500 my-3 text-white px-5 py-2 rounded-xl hover:bg-red-700" onClick={routeToAbout}>
-					About Me
-				</button>
+				<Link href="/about">
+					<button className="w-[175px] text-2xl bg-blue-500 my-3 text-white px-5 py-2 rounded-xl hover:bg-red-700">
+						About Me
+					</button>
+				</Link>
 			</div>
 		</main>
 	);
